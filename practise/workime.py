189d91933b2dc84d518ac01fab28fcac59056692
Jonas Planck <jonas.planck@web.de>
2024-01-26 16:09:22 +0100

    changes

--- practise/workime.py
+++ practise/workime.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def stunden_berechnen(startzeit, endzeit):
+def stunden_berechnen(startzeit, endzeit, pausenzeit):
     # Zeitformat: 'HH:MM'
     format_uhrzeit = '%H:%M'
     
@@ -11,17 +11,24 @@ def stunden_berechnen(startzeit, endzeit):
     # Berechne die Differenz zwischen Endzeit und Startzeit
     arbeitszeit = endzeit_obj - startzeit_obj
     
-    # Gib die Stunden als Dezimalzahl aus
+    # Konvertiere die Pausenzeit in datetime-Objekt
+    pausenzeit_obj = datetime.strptime(pausenzeit, format_uhrzeit)
+    
+    # Subtrahiere die Pausenzeit von der Arbeitszeit
+    arbeitszeit -= pausenzeit_obj
+    
+    # Berechne die Stunden als Dezimalzahl
     stunden = arbeitszeit.total_seconds() / 3600
     
     return stunden
 
-# Nutzereingabe für Startzeit und Endzeit
+# Nutzereingabe für Startzeit, Endzeit und Pausenzeit
 startzeit_input = input("Gib deine Startzeit ein (Format: HH:MM): ")
 endzeit_input = input("Gib deine Endzeit ein (Format: HH:MM): ")
+pausenzeit_input = input("Gib deine Pausenzeit ein (Format: HH:MM): ")
 
 # Berechne die Arbeitsstunden
-gearbeitete_stunden = stunden_berechnen(startzeit_input, endzeit_input)
+gearbeitete_stunden = stunden_berechnen(startzeit_input, endzeit_input, pausenzeit_input)
 
 # Gib das Ergebnis aus
 print("Du hast heute {} Stunden gearbeitet.".format(gearbeitete_stunden))
